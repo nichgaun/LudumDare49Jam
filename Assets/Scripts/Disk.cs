365aa83af49b25337b29220e4387f036d36d3eb1
Jake Fenton <jake@fenton.io>
2021-10-02 13:12:04 -0500

    make spinny CDs make banana number go bigly

--- Assets/Scripts/Disk.cs
+++ Assets/Scripts/Disk.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Disk : MonoBehaviour
 {
     [SerializeField] float angularSpeed; // set in editor
+    Text bananaCountTextField;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        bananaCountTextField = GameObject.FindGameObjectWithTag("UIBananaCount").GetComponent<Text>();
     }
 
     // Update is called once per frame
@@ -23,6 +25,8 @@ public class Disk : MonoBehaviour
     {
         if (other.tag == "Player")
         {
+            StaticInformation.myInt += 1;
+            bananaCountTextField.text = "Banana Count: " + StaticInformation.myInt;
             Destroy(gameObject);
         }
     }
