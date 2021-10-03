6975753d9e146d656d60d1622d4e2a705428a90f
Jake Fenton <jake@fenton.io>
2021-10-03 15:53:17 -0500

    add cop model

---
+++ Assets/Scripts/Cop.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cop : MonoBehaviour
+{
+    [SerializeField] float collidingTumbleSpeed; // set in editor
+    private CollidableObstacle _ourCollidableSelf;
+
+    void Start() {
+        _ourCollidableSelf = GetComponent<CollidableObstacle>();
+    }
+
+    void Update() {}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var impactingCar = other.GetComponent<Car>();
+        if (!impactingCar)
+        {
+            return;
+        }
+
+        // determine relative positioning
+        int _flyawayDirection;
+        if (other.gameObject.transform.position.z < gameObject.transform.position.z) {
+            _flyawayDirection = 1;
+        } else {
+            _flyawayDirection = -1;
+        }
+
+        var relativeSpeed = impactingCar.HSpeed;
+        _ourCollidableSelf.SetVerticalSpeed(relativeSpeed);
+        _ourCollidableSelf.SetHorizontalSpeed(relativeSpeed * _flyawayDirection);
+        _ourCollidableSelf.SetTumbleDirectionAndIntensity(new Vector3(1, 1, 1), collidingTumbleSpeed);
+        SoundPlayer.Play("hitBarricade");
+    }
+}
